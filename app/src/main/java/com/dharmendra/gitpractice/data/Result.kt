560253dca67aa49490609d5b3bdb693b33f942ae
Dharmendra Kumar <kumardakdharmendra@gmail.com>
2022-08-18 08:39:35 +0530

    Some Space is added

--- app/src/main/java/com/dharmendra/gitpractice/data/Result.kt
+++ app/src/main/java/com/dharmendra/gitpractice/data/Result.kt
@@ -3,5 +3,7 @@ package com.dharmendra.gitpractice.data
 sealed class Result<T>{
     data class Loading<T>(val isLoading:Boolean) : Result<T>()
     data class Success<T>(val data :T) : Result<T>()
+
     data class Error<T>(val error:T):Result<T>()
+
 }
