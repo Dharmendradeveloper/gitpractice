dcb9667c5095dbb8ef28be677deeb49470a01118
Dharmendra Kumar <kumardakdharmendra@gmail.com>
2022-08-18 08:42:57 +0530

    Some Space is removed

--- app/src/main/java/com/dharmendra/gitpractice/data/Result.kt
+++ app/src/main/java/com/dharmendra/gitpractice/data/Result.kt
@@ -3,7 +3,6 @@ package com.dharmendra.gitpractice.data
 sealed class Result<T>{
     data class Loading<T>(val isLoading:Boolean) : Result<T>()
     data class Success<T>(val data :T) : Result<T>()
-
     data class Error<T>(val error:T):Result<T>()
 
 }
